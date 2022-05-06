757b6e0601ee5a2b933cc33f97822c2d56d6a3b5
lamptissue <ncarribine@hotmail.co.uk>
2022-05-06 17:55:23 +0100

    Updated pokemonList

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-alert('Hello world');
-
-let favoriteFood = "cheese burger";
-document.write(favoriteFood);
+let pokemonList = [
+                  {Name: "Bulbasaur" , Height: 12, types: ['grass', 'poison', 'ground']},
+                  {Name: "Yellow one" , Height: 8, types: ['electricity', 'smelly', 'ground']},
+                  {Name: "Turtle one" , Height: 15, types: ['water', 'ground']}
+                ];
