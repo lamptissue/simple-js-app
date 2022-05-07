e7eb1b007499b4ad0250dc7108e779e831c1721d
lamptissue <ncarribine@hotmail.co.uk>
2022-05-07 18:07:38 +0100

    for loop included
    for loop prints all the pokemon with their height. The loop also highlights special/large pokemon over the height of 11

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
 let pokemonList = [
-                  {Name: "Bulbasaur" , Height: 12, types: ['grass', 'poison', 'ground']},
-                  {Name: "Yellow one" , Height: 8, types: ['electricity', 'smelly', 'ground']},
-                  {Name: "Turtle one" , Height: 15, types: ['water', 'ground']}
-                ];
+  {name: "Bulbasaur",
+   height: 10,
+   types: ["grass", "poison"]
+  },
+  {name: "Pikachu",
+   height: 4,
+   types: ["electric"]
+  },
+  {name: "Squirtle",
+   height: 13,
+   types: ["water"]}
+  ];
+
+
+for (let i = 0; i < pokemonList.length; i++) {
+  // Looks for large pokemon with a height over 11
+  if (pokemonList[i].height > 11) {
+    document.write("<p>" + pokemonList[i].name + " " + "(height " + pokemonList[i].height + ") - Whoa! That's a big one!</p>");
+  }
+  else {
+    document.write("<p>" + pokemonList[i].name + " " + "(height " + pokemonList[i].height + ") </p>");
+  }
+}
